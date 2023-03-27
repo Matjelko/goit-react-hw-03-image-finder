afc219c95fa4068c2c80b4b085b41dd0e8a5db8b
Matjelko <matjelko11@gmail.com>
2023-03-27 19:34:44 +0200

    Update

---
+++ src/components/App/App.jsx
@@ -0,0 +1,48 @@
+import { Component } from "react";
+// import axios from "axios";
+
+// axios.defaults.baseURL = "https://pixabay.com/api/"
+
+// const ImageGallery = ({ images }) => (
+//   <ul>
+//     {images.map(({ id, webformatURL, largeImageURL }) => (
+//       <li key={id}>
+//         <a href={webformatURL} target="_blank" rel="noreferrer noopener"></a>
+//           {largeImageURL}
+//       </li>
+//     ))}
+//   </ul>
+// )
+
+// class App extends Component{
+//   state = {
+//     images: [],
+//   };
+
+  // async componentDidMount() {
+  //   const response = await axios.get("/search?query=cat")
+  //   this.setState({ articles: response.data.hits });
+  // }
+
+  // render() {
+  //   const { images } = this.state;
+
+  //   return (
+  //     <div>
+  //       <ImageGallery images={images}/>
+  //     </div>
+  //   )
+  // }
+// }
+
+class App extends Component {
+  render(){
+    return(
+      <>
+        <span>Work in progress</span>
+      </>
+    )
+  }
+}
+
+export default App;
